bfc1291f9dd3caa76092411c82c5e8398c57e3ab
abdulahshoaib <khan.abdullah.shoaib@gmail.com>
2025-07-08 13:28:46 +0500

    fix(waitgrp): added the waitgroup to wait before writing to handler data

--- pipeline/processData.go
+++ pipeline/processData.go
@@ -5,32 +5,36 @@ import (
 	"sync"
 )
 
-type ResultWriter func(object_id string, embeddings, triples []string)
+func ProcessFiles(object_id string, memFiles map[string][]byte, writeBack ResultWriter) {
+	var wg = sync.WaitGroup{}
+	var embs [][]float64
+	var trips []string
+	var mutex = sync.Mutex{}
 
-func ProcessFiles(object_id string, memFiles map[string][]byte, writeBack ResultWriter)  {
-		var wg = sync.WaitGroup{}
-		var embs []string
-		var trips []string
-		var mutex = sync.Mutex{}
+	for fname, content := range memFiles {
+		wg.Add(1)
+		go func(fname string, data []byte) {
+			defer wg.Done()
 
-		for name, content := range memFiles {
-			wg.Add(1)
-			go func(name string, data []byte) {
-				defer wg.Done()
+			text := string(data)
+			embeddings, err := embeddingsAPI([]string{text})
+			if err != nil {
+				log.Printf("embedding failed: %v", err)
+			} else {
+				log.Printf("Received embedding of length: %d", len(embeddings[0]))
+			}
 
-				// embedding logic
-				embeddings := ""
-				// triple logic
-				triple := ""
+			triples := ""
 
-				mutex.Lock()
-				embs = append(embs, embeddings)
-				trips = append(trips, triple)
-				mutex.Unlock()
-			}(name, content)
-		}
-		wg.Wait()
-		log.Printf("Processed job %s: %d files", object_id, len(memFiles))
+			mutex.Lock()
+			embs = append(embs, embeddings[0])
+			trips = append(trips, triples)
+			mutex.Unlock()
 
-		writeBack(object_id, embs, trips)
+		}(fname, content)
+	}
+	wg.Wait()
+	log.Printf("Processed job %s: %d files", object_id, len(memFiles))
+
+	writeBack(object_id, embs, trips)
 }
