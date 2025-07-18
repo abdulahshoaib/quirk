fde53d237ba5bf4d835e5e71c19f76783cdcde84
abdulahshoaib <khan.abdullah.shoaib@gmail.com>
2025-07-18 12:23:18 +0500

    feat: fixed logging

--- middleware/logging.go
+++ middleware/logging.go
@@ -1,18 +1,17 @@
 package middleware
 
 import (
-	"net/http"
 	"log/slog"
+	"net/http"
 )
 
 // fn logging(fn http.HandlerFunc) http.HandlerFunc
 //
 // Takes in the Handler Function for a route and logs
 // the route when hit
-func Logging(fn http.HandlerFunc) http.HandlerFunc{
-	return func (w http.ResponseWriter, r *http.Request)  {
-		slog.Info("route mounted", r.URL.Path)
+func Logging(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		slog.Info("route mounted", slog.String("url", r.URL.Path))
 		fn(w, r)
 	}
 }
-
