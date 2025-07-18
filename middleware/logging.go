c43a3f02608e1629fa331bd1035bab697d3a3333
abdulahshoaib <khan.abdullah.shoaib@gmail.com>
2025-07-18 12:27:40 +0500

    Revert "feat: fixed logging"
    This reverts commit fde53d237ba5bf4d835e5e71c19f76783cdcde84.

--- middleware/logging.go
+++ middleware/logging.go
@@ -1,17 +1,18 @@
 package middleware
 
 import (
-	"log/slog"
 	"net/http"
+	"log/slog"
 )
 
 // fn logging(fn http.HandlerFunc) http.HandlerFunc
 //
 // Takes in the Handler Function for a route and logs
 // the route when hit
-func Logging(fn http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		slog.Info("route mounted", slog.String("url", r.URL.Path))
+func Logging(fn http.HandlerFunc) http.HandlerFunc{
+	return func (w http.ResponseWriter, r *http.Request)  {
+		slog.Info("route mounted", r.URL.Path)
 		fn(w, r)
 	}
 }
+
